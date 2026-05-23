9202fc4f1e3bb5d9200b3632fc98b7c2c78ecc71
Excale <dev_excale@hotmail.com>
2026-05-23 23:09:02 +0200

    refactor: Simplify deserialization and serialization for Notion properties

--- src/main/java/dev/excale/barkeeper/notion/property/SelectSerializer.java
+++ src/main/java/dev/excale/barkeeper/notion/property/SelectSerializer.java
@@ -1,15 +1,10 @@
 package dev.excale.barkeeper.notion.property;
 
 import tools.jackson.core.JsonGenerator;
-import tools.jackson.databind.BeanProperty;
-import tools.jackson.databind.DatabindException;
 import tools.jackson.databind.SerializationContext;
-import tools.jackson.databind.ValueSerializer;
 import tools.jackson.databind.ser.std.StdSerializer;
 
 public class SelectSerializer extends StdSerializer<Object> {
-
-	private final Select annotation;
 
 	/**
 	 * Default constructor needed by Jackson
@@ -17,69 +12,23 @@ public class SelectSerializer extends StdSerializer<Object> {
 	@SuppressWarnings("unused")
 	public SelectSerializer() {
 		super(Object.class);
-		this.annotation = null;
-	}
-
-	/**
-	 * Constructor used for contextualization with the @Select annotation
-	 *
-	 * @param annotation @Select annotation
-	 */
-	public SelectSerializer(Select annotation) {
-		super(Object.class);
-		this.annotation = annotation;
-	}
-
-	@Override
-	public ValueSerializer<?> createContextual(
-		SerializationContext ctx,
-		BeanProperty property
-	) throws DatabindException {
-
-		if(property == null) {
-			ctx.reportBadDefinition(
-				handledType(),
-				"Cannot use NotionSelectSerializer outside of a field context. " +
-					"It must be attached to a specific field via @NotionSelect."
-			);
-			return null; // Unreachable
-		}
-
-		// Get the @Select annotation from the property
-		Select selectMeta = property.getAnnotation(Select.class);
-
-		if(selectMeta == null) {
-			ctx.reportBadDefinition(
-				handledType(),
-				"Cannot use NotionSelectSerializer outside of a field context. " +
-					"It must be attached to a specific field via @NotionSelect."
-			);
-			return null; // Unreachable
-		}
-
-		// Create new contextualized serializer
-		return new SelectSerializer(selectMeta);
 	}
 
 	@Override
 	public void serialize(Object value, JsonGenerator gen, SerializationContext ctx) {
 
-		// Check annotation is present
-		if(annotation == null) {
-			ctx.reportBadDefinition(
-				handledType(),
-				"SelectSerializer was not properly contextualized with @Select annotation."
-			);
-			return; // Unreachable
-		}
-
 		// Write the select property structure
 		gen.writeStartObject()
-			.writeName("select")
-				.writeStartObject()
+			.writeName("select");
+
+		if(value != null)
+			gen.writeStartObject()
 				.writeStringProperty("name", value.toString())
-				.writeEndObject()
-		.writeEndObject();
+				.writeEndObject();
+		else
+			gen.writeNull();
+
+		gen.writeEndObject();
 
 	}
 
